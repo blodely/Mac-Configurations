77820b080b4ba3435b8d866c0448324417c92942
骆昱 (Mac mini) <indie.luo@gmail.com>
2016-12-05 11:50:03 +0800

    Add : server shell script

---
+++ server-aliyun-198.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# CREATED BY LUO YU
+# DATE: MONDAY, DECEMBER 5, 2016
+
+# UPDATE
+apt-get update
+
+# UPGRADE
+apt-get upgrade -y
+
+# INSTALL APP
+apt-get install -y nginx mysql-server php5-fpm php5-mysqlnd
+
+
