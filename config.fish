58448ddacdccb350007d421b111079eed06b7c75
Luo Yu <indie.luo@gmail.com>
2019-08-05 14:58:05 +0800

    [Add] fish config

---
+++ config.fish
@@ -0,0 +1,9 @@
+# Created By Luo Yu(indie.luo@gmail.com)
+# Date: August 5, 2019
+
+abbr -a gitpush 'git push && git gc'
+abbr -a gitpull 'git pull && git gc'
+abbr -a gitpullr 'git pull --rebase'
+abbr -a openws 'open *.xcworkspace'
+abbr -a tunnel 'sshuttle'
+
