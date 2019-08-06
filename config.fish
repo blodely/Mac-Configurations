7413105045bf0579922426d2ae80adaf05e11b17
Luo Yu <indie.luo@gmail.com>
2019-08-06 14:03:49 +0800

    [Modify] fish config

--- config.fish
+++ config.fish
@@ -5,5 +5,5 @@ abbr -a gitpush 'git push && git gc'
 abbr -a gitpull 'git pull && git gc'
 abbr -a gitpullr 'git pull --rebase'
 abbr -a openws 'open *.xcworkspace'
-abbr -a tunnel 'sshuttle'
+abbr -a tunnel 'sshuttle --dns -r root@ps.luoyu.space 0/0 -vv 2> /dev/null'
 
