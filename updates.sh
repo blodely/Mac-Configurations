5bf564fbca193a1e48abcf44cd9fbd99c11d4154
Luo Yu <indie.luo@gmail.com>
2020-05-11 12:40:43 +0800

    [Modify] format

--- updates.sh
+++ updates.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 # created by luo yu
 
+echo ""
 echo "Welcome to Updates Script"
-echo "Created By Luo Yu"
-echo "April 10, 2018"
+echo "Created By Luo Yu at April 10, 2018"
 echo ""
-echo "--- Update brew ---"
+echo "--- Update Homebrew ---"
+echo "--- 1. Update brew"
 brew update
-echo ""
-echo "--- Upgrade brew formulas ---"
-brew upgrade
-echo ""
-echo "Cleanup outdated brew formulas"
 brew cleanup
 echo ""
-echo "--- Update pods ---"
+echo "--- 2. Upgrade brew formulas"
+brew upgrade
+brew cleanup
 echo ""
-echo "--- Master repo ---"
-cd ~/.cocoapods/repos/master
+echo "--- 3. Cleanup outdated brew formulas"
+brew cleanup
+echo ""
+echo "--- Update Cocoapods ---"
+echo ""
+echo "--- 1. Master repo ---"
+cd ~/.cocoapods/repos/cocoapods
 git pull --verbose
 echo ""
 git gc
 echo ""
-echo "--- LYSpecs repo ---"
+echo "--- 2. LYSpecs repo ---"
 cd ~/.cocoapods/repos/LYSpecs
 git pull --verbose
 git gc
 echo ""
+echo "--- All ops are finished."
 echo "T H E   E N D."
 echo ""
 exit
