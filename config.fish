393b5e75388b5dab6b50b7d1895db1b4a04013b4
Rick Luo <luoyu@luoyu.space>
2022-01-21 11:55:54 +0800

    [Modify] fish configs

--- config.fish
+++ config.fish
@@ -1,13 +1,38 @@
+#
 # Created By Luo Yu(indie.luo@gmail.com)
 # Date: August 5, 2019
+#
+# MIT License
+#
+# Copyright (c) 2016~2019 骆昱
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# git operators
 abbr -a gitpush 'git push && git gc'
 abbr -a gitpull 'git pull && git gc'
 abbr -a gitpullr 'git pull --rebase'
 
+# xcode
 abbr -a openws 'open *.xcworkspace'
-
-abbr -a tunnel 'sshuttle --dns -r root@ps.luoyu.space 0/0 -vv 2> /dev/null'
-
 abbr -a podinst 'pod install'
+abbr -a podinste 'cd Example && pod install'
 
+# abbr -a tunnel 'sshuttle --dns -r root@ps.luoyu.space 0/0 -vv 2> /dev/null'
