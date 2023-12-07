cb304f6cb3095c34288ed47d74a2448113e6f06f
Luo Yu <luoyu@luoyu.space>
2023-12-07 20:39:13 +0800

    [Fix] image name

--- uppy_node_xhr/run2_container.sh
+++ uppy_node_xhr/run2_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir uploads
-sudo docker run -it -d --rm --name uppy_nodexhr -p 5173:5173 -p 3020:3020 -v ./uploads:/app/examples/node-xhr/uploads rick/uppy-node-xhr:1.0.0
+sudo docker run -it -d --rm --name uppy_nodexhr -p 5173:5173 -p 3020:3020 -v ./uploads:/app/examples/node-xhr/uploads blodely/uppy-node-xhr:1.0.0
