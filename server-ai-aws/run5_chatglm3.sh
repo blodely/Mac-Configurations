6f80dd070548c74b8af1378ae126f536a977fc85
Rick Luo <luoyu@luoyu.space>
2023-12-22 16:07:35 +0800

    [Fix] image name

--- server-ai-aws/run5_chatglm3.sh
+++ server-ai-aws/run5_chatglm3.sh
@@ -2,7 +2,7 @@
 # Created by Luo Yu
 
 cd 
-sudo docker run -it -d --name chatglm3_6b_api -p 8000:8000 -v /data/models/chatglm3-6b:/workspace/THUDM/chatglm3-6b --gpus all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all blodely/chatglm3-6b:1.0.0
+sudo docker run -it -d --name chatglm3_6b_api -p 8000:8000 -v /data/models/chatglm3-6b:/workspace/THUDM/chatglm3-6b --gpus all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all blodely/chatglm3:1.0.0
 
 echo ""
 sudo docker ps -a
